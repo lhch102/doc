579b59389bbc4c0464454eea196e57d897a9cadc
lhch <lhch102@yeah.net>
2018-05-08 18:36:03 +0800

    更新表结构

--- ideal.sql
+++ ideal.sql
@@ -1,31 +1,29 @@
 /*
- Navicat MySQL Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 50721
- Source Host           : localhost
- Source Database       : ideal
+Source Server         : localhost
+Source Server Version : 50720
+Source Host           : localhost:3306
+Source Database       : ideal
 
- Target Server Type    : MySQL
- Target Server Version : 50721
- File Encoding         : utf-8
+Target Server Type    : MYSQL
+Target Server Version : 50720
+File Encoding         : 65001
 
- Date: 05/07/2018 22:10:57 PM
+Date: 2018-05-08 18:35:00
 */
 
-SET NAMES utf8;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
---  Table structure for `comment_info`
+-- Table structure for `comment_info`
 -- ----------------------------
 DROP TABLE IF EXISTS `comment_info`;
 CREATE TABLE `comment_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `movie_id` int(11) NOT NULL COMMENT '影片ID',
   `comment_type` char(1) DEFAULT NULL COMMENT '评论类型：0:短评；1:长评',
-  `comment_nick_name` varchar(50) COMMENT '评论人昵称',
+  `comment_nick_name` varchar(50) DEFAULT NULL COMMENT '评论人昵称',
   `create_id` int(11) DEFAULT NULL COMMENT '创建人ID',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `modify_id` int(11) DEFAULT NULL COMMENT '修改人ID',
@@ -35,7 +33,11 @@ CREATE TABLE `comment_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='影片评价表';
 
 -- ----------------------------
---  Table structure for `dict`
+-- Records of comment_info
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `dict`
 -- ----------------------------
 DROP TABLE IF EXISTS `dict`;
 CREATE TABLE `dict` (
@@ -56,26 +58,31 @@ CREATE TABLE `dict` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='字典表';
 
 -- ----------------------------
---  Table structure for `movie_details`
+-- Records of dict
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `movie`
+-- ----------------------------
+DROP TABLE IF EXISTS `movie`;
+CREATE TABLE `movie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(40) DEFAULT NULL COMMENT '影片名称',
+  `url` varchar(100) DEFAULT NULL,
+  `rate` float DEFAULT NULL COMMENT '评分',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `movie_id_uindex` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of movie
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `movie_details`
 -- ----------------------------
 DROP TABLE IF EXISTS `movie_details`;
 CREATE TABLE `movie_details` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `movie_list_id` int(11) DEFAULT NULL COMMENT '影单ID',
-  `movie_info_id` int(11) DEFAULT NULL COMMENT '影片ID',
-  `create_id` int(11) DEFAULT NULL COMMENT '创建人ID',
-  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-  `modidy_id` int(11) DEFAULT NULL COMMENT '修改人',
-  `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `movie_list_details_id_uindex` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='影单详情表';
-
--- ----------------------------
---  Table structure for `movie_info`
--- ----------------------------
-DROP TABLE IF EXISTS `movie_info`;
-CREATE TABLE `movie_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `chinese_name` varchar(50) DEFAULT NULL COMMENT '影片中文名',
   `english_name` varchar(50) DEFAULT NULL COMMENT '影片英文名',
@@ -101,7 +108,31 @@ CREATE TABLE `movie_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='影片信息表';
 
 -- ----------------------------
---  Table structure for `roles`
+-- Records of movie_details
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `movie_list`
+-- ----------------------------
+DROP TABLE IF EXISTS `movie_list`;
+CREATE TABLE `movie_list` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `movie_list_id` int(11) DEFAULT NULL COMMENT '影单ID',
+  `movie_info_id` int(11) DEFAULT NULL COMMENT '影片ID',
+  `create_id` int(11) DEFAULT NULL COMMENT '创建人ID',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `modidy_id` int(11) DEFAULT NULL COMMENT '修改人',
+  `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `movie_list_details_id_uindex` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='影单详情表';
+
+-- ----------------------------
+-- Records of movie_list
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `roles`
 -- ----------------------------
 DROP TABLE IF EXISTS `roles`;
 CREATE TABLE `roles` (
@@ -117,7 +148,11 @@ CREATE TABLE `roles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
 
 -- ----------------------------
---  Table structure for `staff`
+-- Records of roles
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `staff`
 -- ----------------------------
 DROP TABLE IF EXISTS `staff`;
 CREATE TABLE `staff` (
@@ -135,10 +170,14 @@ CREATE TABLE `staff` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='职员表';
 
 -- ----------------------------
---  Table structure for `user_info`
+-- Records of staff
 -- ----------------------------
-DROP TABLE IF EXISTS `user_info`;
-CREATE TABLE `user_info` (
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `name` varchar(50) NOT NULL COMMENT '真实姓名',
   `nick_name` varchar(50) DEFAULT NULL COMMENT '昵称',
@@ -158,7 +197,11 @@ CREATE TABLE `user_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `user_role`
+-- Records of user
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `user_role`
 -- ----------------------------
 DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role` (
@@ -166,4 +209,6 @@ CREATE TABLE `user_role` (
   `user_id` int(11) DEFAULT NULL COMMENT '用户ID'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户角色表';
 
-SET FOREIGN_KEY_CHECKS = 1;
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
