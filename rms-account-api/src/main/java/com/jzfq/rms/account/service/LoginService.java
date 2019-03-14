0fd0ea17e60a3b39059b7f7b80681e877acf3c93
李虎城 <lihucheng@juzifenqi.com>
2019-03-14 13:35:27 +0800

    软考

--- rms-account-api/src/main/java/com/jzfq/rms/account/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.jzfq.rms.account.service;
-
-import com.jzfq.rms.account.bean.AccountUser;
-import com.jzfq.rms.account.bean.ResponseResult;
-import com.jzfq.rms.account.common.AccountLogin;
-
-public interface LoginService {
-    public ResponseResult login(AccountLogin accountLogin);
-}
--- rms-account-api/src/main/java/com/jzfq/rms/account/utils/SaltUtil.java
+++
@@ -1,5 +0,0 @@
-package com.jzfq.rms.account.utils;
-
-public class SaltUtil {
-    public final static String saltStr="1QAZ2WSX!!";
-}
